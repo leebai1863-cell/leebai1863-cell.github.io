b9388eadcc92e6d50d4340e3f1d1ae00f9759953
leebai1863-cell <leebai1863@gmail.com>
2026-01-09 09:23:56 +0000

    Deploying to gh-pages from @ leebai1863-cell/leebai1863-cell.github.io@bfbb808e5f78453c0d5fbf5814aeb3ea5286255a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-个人简介",
+    title: "个人简介",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
