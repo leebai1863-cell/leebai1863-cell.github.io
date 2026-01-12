c85d04bc0f1f566862a2f9964fe0a7d9bf7f4b0b
leebai1863-cell <leebai1863@gmail.com>
2026-01-12 11:49:30 +0000

    Deploying to gh-pages from @ leebai1863-cell/leebai1863-cell.github.io@6be920ac13ea0c8986aa8caa7f904739b4a8a907 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/news/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-publications",
           title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
           title: "repositories",
